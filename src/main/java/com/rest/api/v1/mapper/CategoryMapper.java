92c97b5deda9111ad9e5164ffec9b88c191e3d4e
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-08-02 21:49:21 -0600

    add mapstruct annotation

--- src/main/java/com/rest/api/v1/mapper/CategoryMapper.java
+++ src/main/java/com/rest/api/v1/mapper/CategoryMapper.java
@@ -3,10 +3,13 @@ package com.rest.api.v1.mapper;
 import com.rest.api.v1.model.CategoryDTO;
 import com.rest.domain.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryMapper {
     CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    @Mapping(source = "id", target = "id")
     CategoryDTO categoryToCategoryDTO(Category category);
 }
